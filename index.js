64d9e980f51c6898d414c6b02e574e6793b95053
Shubham-Tiwar <98662784+Shubham-Tiwar@users.noreply.github.com>
2024-10-24 16:16:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('Hello, this is my node.js containerised apps "\n');
+	res.end('Hello, this is node.js containerised apps "\n');
 });
 const port= 3001;
 server.listen(port, () => {
